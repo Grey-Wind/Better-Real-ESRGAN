d964b66eba18f2bbb9087762708daae1456b137f
Grey-Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-16 07:27:27 +0800

    Update anime_mp4.cmd

--- anime_mp4.cmd
+++ anime_mp4.cmd
@@ -1,5 +1,5 @@
-ffmpeg -i input.mp4 -qscale:v 1 -qmin 1 -qmax 1 -vsync 0 tmp_frames/frame%08d.png
-./realesrgan-ncnn-vulkan.exe -i tmp_frames -o out_frames -n realesr-animevideov3 -s 2 -f jpg
+ffmpeg -i input.mp4 -qscale:v 1 -qmin 1 -qmax 1 -fps_mode 0 tmp_frames/frame%08d.png
+realesrgan-ncnn-vulkan.exe -i tmp_frames -o out_frames -n realesr-animevideov3 -s 2 -f jpg
 ffmpeg -i input.mp4
 ffmpeg -r 23.98 -i out_frames/frame%08d.jpg -c:v libx264 -r 23.98 -pix_fmt yuv420p output.mp4
 pause
