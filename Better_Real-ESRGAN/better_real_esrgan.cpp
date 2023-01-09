44496bc0cc327e8e413eaa522fce5563484e4464
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-09 08:56:35 +0800

    添加点击事件

--- Better_Real-ESRGAN/better_real_esrgan.cpp
+++ Better_Real-ESRGAN/better_real_esrgan.cpp
@@ -6,6 +6,7 @@ Better_Real_ESRGAN::Better_Real_ESRGAN(QWidget *parent)
     , ui(new Ui::Better_Real_ESRGAN)
 {
     ui->setupUi(this);
+    connect(this->ui->Sponsor,SIGNAL(clicked(bool)),this,SLOT(on_Sponsor_clicked()));
 }
 
 Better_Real_ESRGAN::~Better_Real_ESRGAN()
@@ -87,5 +88,7 @@ void Better_Real_ESRGAN::on_Gitee_clicked()
 
 void Better_Real_ESRGAN::on_Sponsor_clicked()
 {
+    sponsor d = *new sponsor ;
+    d.show();
 }
 
