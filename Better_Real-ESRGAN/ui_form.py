f63853dd91aefb9353104e495727cf5491dd72e9
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-10 11:15:35 +0800

    添加标题并修正软件名

--- Better_Real-ESRGAN/ui_form.py
+++ Better_Real-ESRGAN/ui_form.py
@@ -15,13 +15,19 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
 
 class Ui_Better_Real_ESRGAN(object):
     def setupUi(self, Better_Real_ESRGAN):
         if not Better_Real_ESRGAN.objectName():
             Better_Real_ESRGAN.setObjectName(u"Better_Real_ESRGAN")
         Better_Real_ESRGAN.resize(515, 296)
+        self.label = QLabel(Better_Real_ESRGAN)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(180, 0, 211, 31))
+        font = QFont()
+        font.setPointSize(13)
+        self.label.setFont(font)
 
         self.retranslateUi(Better_Real_ESRGAN)
 
@@ -29,6 +35,7 @@ class Ui_Better_Real_ESRGAN(object):
     # setupUi
 
     def retranslateUi(self, Better_Real_ESRGAN):
-        Better_Real_ESRGAN.setWindowTitle(QCoreApplication.translate("Better_Real_ESRGAN", u"Better_Real_ESRGAN", None))
+        Better_Real_ESRGAN.setWindowTitle(QCoreApplication.translate("Better_Real_ESRGAN", u"\u66f4\u597d\u7684Real-ESRGAN", None))
+        self.label.setText(QCoreApplication.translate("Better_Real_ESRGAN", u"\u66f4\u597d\u7684Real-ESRGAN", None))
     # retranslateUi
 
