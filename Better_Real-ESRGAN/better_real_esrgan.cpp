3e51ccda2b5642d9cf9d8bcbc234e858bc989129
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-22 17:54:29 +0800

    删除部分注释

--- Better_Real-ESRGAN/better_real_esrgan.cpp
+++ Better_Real-ESRGAN/better_real_esrgan.cpp
@@ -83,21 +83,6 @@ void Better_Real_ESRGAN::on_Github_clicked()
 
 void Better_Real_ESRGAN::on_Help_clicked()
 {
-    /*QString strInfo;
-    QProcess p(nullptr);
-    QString curPath = QDir::currentPath();//获取当前应用程序目录
-    QString toolpath;
-    toolpath = "/help";    // 当前运行路径下的文件夹（其中是需要调用的.bat
-    curPath.append(toolpath);  //利用append方法在路径字符串后添加文件夹字符串
-    p.setWorkingDirectory(curPath); //设置工作目录
-    toolpath = "/help.bat";  //要调用的脚本文件
-    curPath.append(toolpath); //添加脚本文件字符串
-    p.start(curPath);  //运行脚本文件
-    if(p.waitForFinished()){      //等待脚本运行完成，超时时间默认是3000s,超时返回0，正常返回1
-        strInfo = "完成！";
-    }else{
-        strInfo = "bat运行错误！";
-    }*/
     system("start help.chm");
 }
 
