4e12d193433fb1fbd3af574e7caac30be297f5c5
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-22 18:00:03 +0800

    重构项目以使用新的UI

---
+++ build-Better_Real-ESRGAN-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Qt/6.4.2/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Qt/6.4.2/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/Qt/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/Qt/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
