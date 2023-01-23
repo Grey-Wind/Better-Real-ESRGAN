661c2f950e354cdb3769b799cdc56ede30f8cd0a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-23 22:26:42 +0800

    完成动漫jpg

--- Better_Real-ESRGAN/command/VisualBasic/AnimeJpg/Program.vb
+++
@@ -1,7 +0,0 @@
-Imports System
-
-Module Program
-    Sub Main(args As String())
-        Shell("cmd.exe /c realesrgan-ncnn-vulkan.exe -i input.jpg -o output.png -n realesrgan-x4plus-anime")
-    End Sub
-End Module
