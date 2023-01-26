c5c160ba83cb73922b1c92a93efea770b5befed1
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-26 15:39:38 +0800

    核心软件加入更多构建器

---
+++ build-Better_Real-ESRGAN-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Qt/5.15.2/mingw81_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Qt/5.15.2/mingw81_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/Qt/Tools/mingw810_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/Qt/Tools/mingw810_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
