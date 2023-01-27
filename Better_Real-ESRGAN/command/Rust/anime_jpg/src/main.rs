296e413fd32a3f9bfc25258b099438c54cdbbc7a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-27 15:14:29 +0800

    填入参数

--- Better_Real-ESRGAN/command/Rust/anime_jpg/src/main.rs
+++ Better_Real-ESRGAN/command/Rust/anime_jpg/src/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Test");
+    Command::new("realesrgan-ncnn-vulkan.exe")
+        .arg("-i")
+        .arg("input.jpg")
+        .arg("-o")
+        .arg("output.png")
+        .arg("-n")
+        .arg("realesrgan-x4plus-anime");
 }
