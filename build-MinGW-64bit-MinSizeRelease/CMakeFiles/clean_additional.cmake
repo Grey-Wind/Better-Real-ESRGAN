22d262730b56e783c4da318a5992d69b202b7e4a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-27 11:07:22 +0800

    全面重调项目构建套件设置

---
+++ build-MinGW-64bit-MinSizeRelease/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "MinSizeRel")
+  file(REMOVE_RECURSE
+  "Better_Real-ESRGAN_autogen"
+  "CMakeFiles\\Better_Real-ESRGAN_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\Better_Real-ESRGAN_autogen.dir\\ParseCache.txt"
+  )
+endif()
