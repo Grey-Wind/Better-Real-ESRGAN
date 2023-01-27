ea04c4727e92087cc9507b7a2b7a1eb229ca62d9
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-27 15:25:37 +0800

    完成AnimeJpg

--- Better_Real-ESRGAN/command/Rust/anime_jpg/src/main.rs
+++ Better_Real-ESRGAN/command/Rust/anime_jpg/src/main.rs
@@ -1,12 +1,15 @@
 use std::process::Command;
 
 fn main() {
-    println!("Test");
-    Command::new("realesrgan-ncnn-vulkan.exe")
+    let _ = Command::new("cmd.exe")
+        .arg("/c")
+        .arg("realesrgan-ncnn-vulkan.exe")
         .arg("-i")
         .arg("input.jpg")
         .arg("-o")
         .arg("output.png")
         .arg("-n")
-        .arg("realesrgan-x4plus-anime");
+        .arg("realesrgan-x4plus-anime")
+        .status();
+    let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
