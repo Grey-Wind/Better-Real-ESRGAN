42daac7fadc5957e6557a87c33f9bc834698399c
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-30 15:58:03 +0800

    Update main.rs

--- Better_Real-ESRGAN/command/Rust/default_jpg/src/main.rs
+++ Better_Real-ESRGAN/command/Rust/default_jpg/src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let _ = Command::new("cmd.exe")
+        .arg("/c")
+        .arg("realesrgan-ncnn-vulkan.exe")
+        .arg("-i")
+        .arg("input.jpg")
+        .arg("-o")
+        .arg("output.png")
+        .arg("-n")
+        .arg("realesrgan-x4plus")
+        .status();
 }
