b68698a5f1b065bf635028e33ec199206a2d7ced
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-30 16:36:20 +0800

    GeneralX4V3构建

--- Better_Real-ESRGAN/command/Rust/GeneralX4V3/src/main.rs
+++ Better_Real-ESRGAN/command/Rust/GeneralX4V3/src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let _ = Command::new("cmd.exe")
+        .arg("/c")
+        .arg("realesrgan-ncnn-vulkan.exe")
+        .arg("-i")
+        .arg("input.png")
+        .arg("-o")
+        .arg("output.png")
+        .arg("-n")
+        .arg("realesr-general-x4v3")
+        .status();
 }
