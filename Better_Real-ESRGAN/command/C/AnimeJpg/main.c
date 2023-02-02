32b7654bed2f68bae1e946e01944f0f70fa8abda
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-02-02 21:12:03 +0800

    修复后缀名错误

--- Better_Real-ESRGAN/command/C/AnimeJpg/main.c
+++ Better_Real-ESRGAN/command/C/AnimeJpg/main.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
 
 int main(){
-	system("realesrgan-ncnn-vulkan.exe -i input.png -o output.png -n realesrgan-x4plus-anime");
+	system("realesrgan-ncnn-vulkan.exe -i input.jpg -o output.png -n realesrgan-x4plus-anime");
 }
