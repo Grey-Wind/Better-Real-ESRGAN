428ae3b4758b27bf2614e3f65923bdaa79378baf
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-03-02 22:05:24 +0800

    按钮位置固定

--- updater/updater/MainForm.Designer.cs
+++ updater/updater/MainForm.Designer.cs
@@ -36,11 +36,11 @@
             this.C_Sharp = new System.Windows.Forms.Button();
             this.C_Sharp_NET = new System.Windows.Forms.Button();
             this.Python = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.Java_Maven = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
+            this.Java_Intellij = new System.Windows.Forms.Button();
+            this.Java_Gradle_Groovy = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Cpp
@@ -76,7 +76,7 @@
             // VisualBasic
             // 
             this.VisualBasic.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.VisualBasic.Location = new System.Drawing.Point(480, 12);
+            this.VisualBasic.Location = new System.Drawing.Point(12, 57);
             this.VisualBasic.Name = "VisualBasic";
             this.VisualBasic.Size = new System.Drawing.Size(150, 40);
             this.VisualBasic.TabIndex = 3;
@@ -86,7 +86,7 @@
             // C_Sharp
             // 
             this.C_Sharp.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.C_Sharp.Location = new System.Drawing.Point(12, 58);
+            this.C_Sharp.Location = new System.Drawing.Point(168, 58);
             this.C_Sharp.Name = "C_Sharp";
             this.C_Sharp.Size = new System.Drawing.Size(150, 40);
             this.C_Sharp.TabIndex = 4;
@@ -96,7 +96,7 @@
             // C_Sharp_NET
             // 
             this.C_Sharp_NET.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.C_Sharp_NET.Location = new System.Drawing.Point(168, 58);
+            this.C_Sharp_NET.Location = new System.Drawing.Point(324, 57);
             this.C_Sharp_NET.Name = "C_Sharp_NET";
             this.C_Sharp_NET.Size = new System.Drawing.Size(150, 40);
             this.C_Sharp_NET.TabIndex = 5;
@@ -106,73 +106,73 @@
             // Python
             // 
             this.Python.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.Python.Location = new System.Drawing.Point(324, 58);
+            this.Python.Location = new System.Drawing.Point(12, 103);
             this.Python.Name = "Python";
             this.Python.Size = new System.Drawing.Size(150, 40);
             this.Python.TabIndex = 6;
             this.Python.Text = "Python";
             this.Python.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // Java_Maven
             // 
-            this.button6.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button6.Location = new System.Drawing.Point(480, 58);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(150, 40);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.Java_Maven.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Java_Maven.Location = new System.Drawing.Point(168, 104);
+            this.Java_Maven.Name = "Java_Maven";
+            this.Java_Maven.Size = new System.Drawing.Size(150, 40);
+            this.Java_Maven.TabIndex = 7;
+            this.Java_Maven.Text = "Java Maven";
+            this.Java_Maven.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button7.Location = new System.Drawing.Point(12, 104);
+            this.button7.Location = new System.Drawing.Point(168, 150);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(150, 40);
             this.button7.TabIndex = 8;
-            this.button7.Text = "button7";
+            this.button7.Text = "NONE";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button8.Location = new System.Drawing.Point(168, 104);
+            this.button8.Location = new System.Drawing.Point(324, 149);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(150, 40);
             this.button8.TabIndex = 9;
-            this.button8.Text = "button8";
+            this.button8.Text = "NONE";
             this.button8.UseVisualStyleBackColor = true;
             // 
-            // button9
+            // Java_Intellij
             // 
-            this.button9.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button9.Location = new System.Drawing.Point(324, 104);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(150, 40);
-            this.button9.TabIndex = 10;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
+            this.Java_Intellij.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Java_Intellij.Location = new System.Drawing.Point(324, 103);
+            this.Java_Intellij.Name = "Java_Intellij";
+            this.Java_Intellij.Size = new System.Drawing.Size(150, 40);
+            this.Java_Intellij.TabIndex = 10;
+            this.Java_Intellij.Text = "Java Intellij";
+            this.Java_Intellij.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // Java_Gradle_Groovy
             // 
-            this.button10.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button10.Location = new System.Drawing.Point(480, 104);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(150, 40);
-            this.button10.TabIndex = 11;
-            this.button10.Text = "button10";
-            this.button10.UseVisualStyleBackColor = true;
+            this.Java_Gradle_Groovy.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Java_Gradle_Groovy.Location = new System.Drawing.Point(12, 149);
+            this.Java_Gradle_Groovy.Name = "Java_Gradle_Groovy";
+            this.Java_Gradle_Groovy.Size = new System.Drawing.Size(150, 40);
+            this.Java_Gradle_Groovy.TabIndex = 11;
+            this.Java_Gradle_Groovy.Text = "Java Gradle Groovy";
+            this.Java_Gradle_Groovy.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 155);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
+            this.ClientSize = new System.Drawing.Size(485, 200);
+            this.Controls.Add(this.Java_Gradle_Groovy);
+            this.Controls.Add(this.Java_Intellij);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
+            this.Controls.Add(this.Java_Maven);
             this.Controls.Add(this.Python);
             this.Controls.Add(this.C_Sharp_NET);
             this.Controls.Add(this.C_Sharp);
@@ -183,7 +183,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Better Real-ESRGAN Updater and Downloader";
+            this.Text = "Update/Download";
             this.ResumeLayout(false);
 
         }
@@ -197,11 +197,11 @@
         private System.Windows.Forms.Button C_Sharp;
         private System.Windows.Forms.Button C_Sharp_NET;
         private System.Windows.Forms.Button Python;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Java_Maven;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button Java_Intellij;
+        private System.Windows.Forms.Button Java_Gradle_Groovy;
     }
 }
 
