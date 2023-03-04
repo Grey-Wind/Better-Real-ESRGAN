8efecd17886ac688a20398cb0344c9cdef5bf8fd
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-03-04 15:39:38 +0800

    ui制作完毕

--- updater/ui_form.py
+++ updater/ui_form.py
@@ -21,7 +21,7 @@ class Ui_updater(object):
     def setupUi(self, updater):
         if not updater.objectName():
             updater.setObjectName(u"updater")
-        updater.resize(800, 600)
+        updater.resize(532, 131)
         updater.setWindowTitle(u"updater")
         self.cpp = QPushButton(updater)
         self.cpp.setObjectName(u"cpp")
@@ -29,6 +29,27 @@ class Ui_updater(object):
         self.visual_basic = QPushButton(updater)
         self.visual_basic.setObjectName(u"visual_basic")
         self.visual_basic.setGeometry(QRect(140, 10, 120, 30))
+        self.c_ = QPushButton(updater)
+        self.c_.setObjectName(u"c_")
+        self.c_.setGeometry(QRect(270, 10, 120, 30))
+        self.c_sharp = QPushButton(updater)
+        self.c_sharp.setObjectName(u"c_sharp")
+        self.c_sharp.setGeometry(QRect(400, 10, 120, 30))
+        self.c_sharp_net = QPushButton(updater)
+        self.c_sharp_net.setObjectName(u"c_sharp_net")
+        self.c_sharp_net.setGeometry(QRect(10, 50, 120, 30))
+        self.rust = QPushButton(updater)
+        self.rust.setObjectName(u"rust")
+        self.rust.setGeometry(QRect(140, 50, 120, 30))
+        self.java_maven = QPushButton(updater)
+        self.java_maven.setObjectName(u"java_maven")
+        self.java_maven.setGeometry(QRect(270, 50, 120, 30))
+        self.java_intellij = QPushButton(updater)
+        self.java_intellij.setObjectName(u"java_intellij")
+        self.java_intellij.setGeometry(QRect(400, 50, 120, 30))
+        self.java_gradle_groovy = QPushButton(updater)
+        self.java_gradle_groovy.setObjectName(u"java_gradle_groovy")
+        self.java_gradle_groovy.setGeometry(QRect(10, 90, 251, 30))
 
         self.retranslateUi(updater)
 
@@ -38,6 +59,13 @@ class Ui_updater(object):
     def retranslateUi(self, updater):
         self.cpp.setText(QCoreApplication.translate("updater", u"C++", None))
         self.visual_basic.setText(QCoreApplication.translate("updater", u"Visual Basic", None))
+        self.c_.setText(QCoreApplication.translate("updater", u"C", None))
+        self.c_sharp.setText(QCoreApplication.translate("updater", u"C#", None))
+        self.c_sharp_net.setText(QCoreApplication.translate("updater", u"C#.NET", None))
+        self.rust.setText(QCoreApplication.translate("updater", u"Rust", None))
+        self.java_maven.setText(QCoreApplication.translate("updater", u"Java-Maven", None))
+        self.java_intellij.setText(QCoreApplication.translate("updater", u"Java-Intellij", None))
+        self.java_gradle_groovy.setText(QCoreApplication.translate("updater", u"Java-Gradle-Groovy", None))
         pass
     # retranslateUi
 
