18af0aa843b4084d6a4e11d8ee5f05b4cf31eb56
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-03-04 13:57:14 +0800

    修改项目为pyqt

---
+++ updater/ui_form.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+
+class Ui_updater(object):
+    def setupUi(self, updater):
+        if not updater.objectName():
+            updater.setObjectName(u"updater")
+        updater.resize(800, 600)
+
+        self.retranslateUi(updater)
+
+        QMetaObject.connectSlotsByName(updater)
+    # setupUi
+
+    def retranslateUi(self, updater):
+        updater.setWindowTitle(QCoreApplication.translate("updater", u"updater", None))
+    # retranslateUi
+
