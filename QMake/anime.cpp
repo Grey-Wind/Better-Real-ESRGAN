b295119388282587503a18a57bc2c08c4324c999
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-03-17 22:34:52 +0800

    添加代码

--- QMake/anime.cpp
+++ QMake/anime.cpp
@@ -1,6 +1,8 @@
 #include "anime.h"
 #include "ui_anime.h"
 
+#include <stdlib.h>
+
 Anime::Anime(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Anime)
@@ -12,3 +14,21 @@ Anime::~Anime()
 {
     delete ui;
 }
+
+void Anime::on_PNG_clicked()
+{
+    system("start AnimePng.exe");
+}
+
+
+void Anime::on_JPG_clicked()
+{
+    system("start AnimeJpg.exe");
+}
+
+
+void Anime::on_JPEG_clicked()
+{
+    system("start AnimeJpeg.exe");
+}
+
