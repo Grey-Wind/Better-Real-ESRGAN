d85d56642ee1beaf650e0ad4e57ebf20ccd67035
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-03-19 14:26:18 +0800

    添加default jpeg优化

--- QMake/default.cpp
+++ QMake/default.cpp
@@ -26,3 +26,9 @@ void Default::on_JPG_clicked()
     system("realesrgan-ncnn-vulkan.exe -i input.jpg -o output.png -n realesrgan-x4plus-anime");
 }
 
+
+void Default::on_JPEG_clicked()
+{
+    system("realesrgan-ncnn-vulkan.exe -i input.jpeg -o output.png -n realesrgan-x4plus-anime");
+}
+
