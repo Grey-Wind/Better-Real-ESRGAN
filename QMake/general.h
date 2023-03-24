fa713a7d84ac9e9a67614401daa75e25705092a0
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-03-24 21:06:46 +0800

    release构建，准备发布

--- QMake/general.h
+++ QMake/general.h
@@ -15,6 +15,19 @@ public:
     explicit General(QWidget *parent = nullptr);
     ~General();
 
+private slots:
+    void on_DefaultPng_clicked();
+
+    void on_DefaultJpg_clicked();
+
+    void on_DefaultJpeg_clicked();
+
+    void on_wdnPng_clicked();
+
+    void on_wdnJpg_clicked();
+
+    void on_wdnJpeg_clicked();
+
 private:
     Ui::General *ui;
 };
