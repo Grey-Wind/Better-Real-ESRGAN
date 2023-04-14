fd713a8703d7ce6c45f007dff8b29a5a0b613573
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-04-14 22:08:58 +0800

    写入命令测试

--- Better Real-ESRGAN/MainWindow.xaml.cs
+++ Better Real-ESRGAN/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,14 @@ namespace Better_Real_ESRGAN
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void AnimeJPG_Click(object sender, RoutedEventArgs e)
+        {
+            Process app = new Process();
+            app.StartInfo.FileName = "realesrgan-ncnn-vulkan.exe";
+            app.StartInfo.Arguments = "-i input.jpg -o output.png -n realesrgan-x4plus-anime";
+            app.Start();
         }
     }
 }
