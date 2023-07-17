343326b9d8100e1e10e91c5fe3f219e3761ccf0d
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-17 11:27:10 +0800

    准备重做

--- Better Real-ESRGAN/MainWindow.xaml.cs
+++ Better Real-ESRGAN/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,20 +17,11 @@ namespace Better_Real_ESRGAN
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class Window1 : Window
+    public partial class MainWindow : Window
     {
-        public Window1()
+        public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void AnimeJPG_Click(object sender, RoutedEventArgs e)
-        {
-            Process app = new Process();
-            app.StartInfo.FileName = "realesrgan-ncnn-vulkan.exe";
-            app.StartInfo.Arguments = "-i input.jpg -o output.png -n realesrgan-x4plus-anime";
-            app.Start();
-        }
-
     }
 }
