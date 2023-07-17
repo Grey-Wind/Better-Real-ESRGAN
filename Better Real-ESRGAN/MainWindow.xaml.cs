b4c925a787f27173daa47a5e8f282865ef010bd2
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-17 08:44:45 +0800

    合并内容 (#47)
    * 再次导入项目
    
    * 写入命令测试
    
    * 微调按钮大小
    
    测试通过
    
    * UI小修改
    
    * Create hosts
    
    * Delete hosts
    
    * 删除构建

---
+++ Better Real-ESRGAN/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Better_Real_ESRGAN
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimeJPG_Click(object sender, RoutedEventArgs e)
+        {
+            Process app = new Process();
+            app.StartInfo.FileName = "realesrgan-ncnn-vulkan.exe";
+            app.StartInfo.Arguments = "-i input.jpg -o output.png -n realesrgan-x4plus-anime";
+            app.Start();
+        }
+
+    }
+}
