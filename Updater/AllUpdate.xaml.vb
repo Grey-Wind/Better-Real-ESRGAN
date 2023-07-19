3b6766ef7e8f089b302ffa8d1ce4b2beef9095c7
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-19 16:29:09 +0800

    添加一键更新窗口

---
+++ Updater/AllUpdate.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class AllUpdate
+
+End Class
--- Updater/MainWindow.xaml.vb
+++ Updater/MainWindow.xaml.vb
@@ -1,3 +1,19 @@
-﻿Class MainWindow
+﻿Imports System.ComponentModel
+
+Class MainWindow
+
+    Dim childWindows As New List(Of Window)() '用于存储子窗口引用的列表
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs) ' 用于加载新窗口
+        Dim Allupdate As New AllUpdate()
+        childWindows.Add(Allupdate)
+        Allupdate.Show()
+    End Sub
+
+    Private Sub MainWindow_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing ' 主窗口关闭事件，循环遍历子窗口列表并关闭每个子窗口
+        For Each childWindow As Window In childWindows
+            childWindow.Close()
+        Next
+    End Sub
 
 End Class
