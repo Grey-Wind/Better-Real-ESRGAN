594c8c9bd389cd43756f9ded946f6d1fba4bb24e
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-19 14:07:35 +0800

    完成执行部分+发布并构建

--- Better Real-ESRGAN/MainWindow.xaml.cs
+++ Better Real-ESRGAN/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Windows;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Security.Cryptography;
 using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Better_Real_ESRGAN
 {
@@ -59,14 +62,77 @@ namespace Better_Real_ESRGAN
 
         private void RunBtn_Click(object sender, RoutedEventArgs e)
         {
-            SetParameters();
-        }
-
-        private void SetParameters()
-        {
+            // 设置命令参数
+            string command = "realesrgan-ncnn-vulkan.exe";
+            string inputArgument = "-i";
             string imagePath = ImagePathBox.Text;
+            string outputArgument = "-o";
             string outputPath = OutputPathBox.Text;
+            string modelArgument = "-n";
             string model = SelectModel.Text;
+
+            string arguments = $"{inputArgument} {imagePath} {outputArgument} {outputPath} {modelArgument} {model}";
+
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = command;
+            processInfo.Arguments = arguments;
+            processInfo.RedirectStandardOutput = true;
+            processInfo.RedirectStandardError = true;
+            processInfo.UseShellExecute = false;
+            processInfo.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = processInfo;
+
+            // 初始化 outputBuilder
+            StringBuilder outputBuilder = new StringBuilder();
+
+            process.OutputDataReceived += (eventSender, eventArgs) =>
+            {
+                if (!string.IsNullOrEmpty(eventArgs.Data))
+                {
+                    outputBuilder.AppendLine(eventArgs.Data);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        CommandOutput.Text = outputBuilder.ToString();
+                    });
+                }
+            };
+
+            process.ErrorDataReceived += (eventSender, eventArgs) =>
+            {
+                if (!string.IsNullOrEmpty(eventArgs.Data))
+                {
+                    outputBuilder.AppendLine(eventArgs.Data);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        CommandOutput.Text = outputBuilder.ToString();
+                    });
+                }
+            };
+
+            // 异步执行命令
+            Task.Run(() =>
+            {
+                // 启动进程并开始异步读取输出和错误流
+                process.Start();
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                // 等待进程退出
+                process.WaitForExit();
+
+                // 更新 CommandOutput TextBox
+                Dispatcher.Invoke(() =>
+                {
+                    CommandOutput.Text = outputBuilder.ToString();
+                    CommandOutput.ScrollToEnd(); // 将滚动条滚动到最下方
+                });
+            });
+
         }
     }
 }
--- Better Real-ESRGAN/Properties/AssemblyInfo.cs
+++ Better Real-ESRGAN/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Better Real-ESRGAN")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sunrise Studio")]
 [assembly: AssemblyProduct("Better Real-ESRGAN")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("版权所有 © 2023 Sunrise Studio")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.0.0.0")]
+[assembly: AssemblyFileVersion("9.0.0.0")]
