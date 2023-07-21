ea41d8d765d07925d88aaf0afc0b26f3a462f030
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-21 23:31:00 +0800

    优化窗口结构

--- Updater/MainWindow.xaml.vb
+++ Updater/MainWindow.xaml.vb
@@ -1,13 +1,24 @@
 ﻿Imports System.ComponentModel
+Imports MahApps.Metro.Controls
 
 Class MainWindow
 
+    Inherits Window
+
     Dim childWindows As New List(Of Window)() '用于存储子窗口引用的列表
 
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs) ' 用于加载新窗口
-        Dim Allupdate As New AllUpdate()
-        childWindows.Add(Allupdate)
-        Allupdate.Show()
+    Public Sub New()
+        InitializeComponent()
+        ' Me.ResizeMode = ResizeMode.NoResize ' 禁止最小化
+        Me.ResizeMode = ResizeMode.CanMinimize ' 允许最小化，但有最大化按钮
+        Me.WindowStartupLocation = WindowStartupLocation.CenterScreen
+        ' HideMaximizeButton(Me)
+    End Sub
+
+    Private Sub HideMaximizeButton(window As MetroWindow)
+        window.UseNoneWindowStyle = True
+        window.IgnoreTaskbarOnMaximize = True
+        window.ShowMaxRestoreButton = False
     End Sub
 
     Private Sub MainWindow_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing ' 主窗口关闭事件，循环遍历子窗口列表并关闭每个子窗口
@@ -16,4 +27,9 @@ Class MainWindow
         Next
     End Sub
 
+    Private Sub AllUpdateBtn_Click(sender As Object, e As RoutedEventArgs) Handles AllUpdateBtn.Click
+        Dim Allupdate As New AllUpdate()
+        childWindows.Add(Allupdate)
+        Allupdate.Show()
+    End Sub
 End Class
