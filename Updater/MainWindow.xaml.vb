a1019897625c81af2bc8c5ce26180740d9212496
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-23 23:14:58 +0800

    完成线上检测

--- Updater/MainWindow.xaml.vb
+++ Updater/MainWindow.xaml.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.ComponentModel
 Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
 
 Class MainWindow
 
@@ -13,12 +15,16 @@ Class MainWindow
         Me.ResizeMode = ResizeMode.CanMinimize ' 允许最小化，但有最大化按钮
         Me.WindowStartupLocation = WindowStartupLocation.CenterScreen ' 屏幕居中显示
 
-        ' 版本检测
+        ' 本地版本检测
         LocalVersion.Text = GetLocalFileVersion("./Better Real-ESRGAN.exe")
 
-        ' 暂时禁用检测
-        GitHubVersion.Text = "功能未制作"
-        GiteeVersion.Text = "功能未制作"
+        ' 线上版本检测
+        GitHubVersion.Text = OnlineVersion("https://github.com/Grey-Wind/File/raw/main/Better.Real-ESRGAN/version.txt")
+        GiteeVersion.Text = OnlineVersion("https://gitee.com/sunrise-studio/File/raw/main/Better.Real-ESRGAN/version.txt")
+
+        ' 暂时禁用线上检测
+        ' GitHubVersion.Text = "功能未制作"
+        ' GiteeVersion.Text = "功能未制作"
     End Sub
 
     Private Function GetLocalFileVersion(filePath As String) As String
@@ -29,6 +35,42 @@ Class MainWindow
             Return "N/A" ' 文件未找到时返回一个默认的版本号，例如"N/A"
         End If
     End Function
+
+    ' OnlineVersion 方法
+    Private Function OnlineVersion(ByVal onlineFileUrl As String) As String
+        Dim versionInfo As String = ""
+
+        Try
+            ' 创建 WebRequest 对象
+            Dim request As WebRequest = WebRequest.Create(onlineFileUrl)
+
+            ' 发起 GET 请求
+            Using response As WebResponse = request.GetResponse()
+                Using stream As Stream = response.GetResponseStream()
+                    ' 保存文件到本地
+                    Dim filePath As String = "temp.txt"
+                    Using fileStream As New FileStream(filePath, FileMode.Create)
+                        stream.CopyTo(fileStream)
+                    End Using
+
+                    ' 读取文件内容
+                    Using reader As New StreamReader(filePath)
+                        versionInfo = reader.ReadToEnd()
+                    End Using
+
+                    ' 删除临时文件
+                    File.Delete(filePath)
+                End Using
+            End Using
+        Catch ex As Exception
+            ' 处理异常情况
+            MessageBox.Show("无法获取文件：" & ex.Message)
+        End Try
+
+        Return versionInfo
+    End Function
+
+
 
     Private Sub MainWindow_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing ' 主窗口关闭事件，循环遍历子窗口列表并关闭每个子窗口
         For Each childWindow As Window In childWindows
